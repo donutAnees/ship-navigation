474010daa73fdf0ce3fb573bc7c8410349f7ba40
donutAnees <pvmohammedanees2003@gmail.com>
2024-08-25 20:55:47 +0530

    Format calculatefuel

--- calculatefuel.py
+++ calculatefuel.py
@@ -1,13 +1,12 @@
-"""
-This function calculates the fuel consumption of a given grid.
-Attributes:
-    v: speed of the ship
-    l: length of the voyage distance
-    K: fuel consumption rate of the ship
-    P: power of the main engine
-    C: cost of fuel per ton.
-"""
 def gridconsumption(v, l, K, P, C):
+    """
+    This function calculates the fuel consumption of a given grid.
+    Attributes:
+        v: speed of the ship
+        l: length of the voyage distance
+        K: fuel consumption rate of the ship
+        P: power of the main engine
+        C: cost of fuel per ton.
+    """
     consumption = l / v * (K * P * C)
     return consumption
-
